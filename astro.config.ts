af7688f670970b87ccf4692cbd2063452139f740
Aseem Muhammad <asaseemas@gmail.com>
2023-08-30 23:44:14 +0530

    feat: updated to 3

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 
@@ -21,8 +20,6 @@ export default defineConfig({
 		config: {
 			applyBaseStyles: false
 		}
-	}), image({
-		serviceEntryPoint: "@astrojs/image/sharp"
 	}), sitemap(), prefetch(),
 	partytown({
 		config: {
